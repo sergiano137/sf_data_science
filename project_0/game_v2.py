fc1f8402c08aa1fdc481258a4a3cb1701ac72114
Serg <sergianosan@gmail.com>
2021-10-08 21:40:16 +0300

    v3

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -13,17 +13,19 @@ def random_predict(number:int=1) -> int:
         int: Число попыток
     """
     count = 0
-    low_border = 1 #нижняя граница
-    upper_border = 100 #верхняя граница
+    low_border = 1 #нижняя граница предполагаемого числа
+    upper_border = 100 #верхняя граница предполагаемого числа
     predict_number = np.random.randint(1,101) #предполагаемое число
     while True:
-        count += 1
-        if number > predict_number:
-            low_border = predict_number
-        elif number < predict_number: 
-            upper_border = predict_number
-        predict_number = round((low_border + upper_border)/2)           
-        return count
+        if number != predict_number:
+            count += 1
+            if number > predict_number:
+                low_border = predict_number                 
+            elif number < predict_number: 
+                upper_border = predict_number            
+            predict_number = round((low_border + upper_border)/2) 
+        else: break          
+    return count
 
 def score_game(random_predict) ->int:
     """Функция измерения средненго кол-во попыток угадывания за 1000 попыток
